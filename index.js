7d262c88dc8fe1cae97038a6ce61f436b1ab396e
Harrison-Furg <1fergusonhar@hdsb.ca>
2024-12-02 11:40:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Here is where you put all your code
+
+// The console.log() function will print whatever you
+// put in the brackets in the websites console
+
+
+
+
+// Variables can be declaired to hold numbers or words.
+// They can be put into the console.log() functions to be printed
+
+
+
+
+// If() functions check to see if the calculations within the 
+// brackets are true or not and activate based on that
+
+
+
+
